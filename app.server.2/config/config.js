7f307574d305f45f26847932927573e37af55704
Nivesh Singh <nivesh2@gmail.com>
2016-09-30 08:58:38 +0530

    app.server.2 final commit

--- app.server.2/config/config.js
+++ app.server.2/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port:8083,
-    db:'mongodb://nivesh:just_4_now@ds055584.mlab.com:55584/api',
+    db:'mongodb://nivesh:just_4_now@ds055584.mongolab.com:55584/api',
     url:'http://localhost:8082'
 };
